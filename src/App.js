f901330fe8d349b3f79f9ec0425bcd6833733464
Amina Rahman Nisha <aminarahman01@gmail.com>
2022-12-25 19:26:04 +0600

    basic card-list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -16,8 +17,12 @@ class App extends Component {
   }
   render () {
     return (
-      <div className="App">
+      
+      <div className="App">  
+        <CardList name='Nisha'>
           {this.state.monsters.map(m => <h1 key={m.id}>{m.name}</h1>)}
+        </CardList>  
+        
       </div>
     )
   }
